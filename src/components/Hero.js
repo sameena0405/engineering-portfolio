b1f87c6bc0c0d5f2d0b8692ff9561679f09bc2fb
SameenaFarwej <sameenaparves0405@gmail.com>
2026-05-22 11:19:18 +0530

    fixed resume link path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,7 +27,7 @@ export default function Hero() {
             View Projects
           </a>
 
-          <a href="/resume.pdf" className="btn-outline" download>
+          <a href="resume.pdf" className="btn-outline" download>
             Download Resume
           </a>
         </div>
